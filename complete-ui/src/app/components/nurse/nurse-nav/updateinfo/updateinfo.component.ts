b269c8731576583c3ada35801a87bf6ca54f22db
YashuGv <venkatayasaswini66@gmail.com>
2023-03-23 21:03:19 +0530

    PHR post method implemented

--- complete-ui/src/app/components/nurse/nurse-nav/updateinfo/updateinfo.component.ts
+++ complete-ui/src/app/components/nurse/nurse-nav/updateinfo/updateinfo.component.ts
@@ -1,64 +1,74 @@
-import { Component,Inject } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
-import { AppointmentServiceService } from 'src/app/services/appointment-service/appointment-service.service';
-import { Guid } from 'guid-typescript';
-import { localStorageToken } from '../../../patient/show-doctors/localstorage.token';
-import { AppointmentDoctor } from 'src/app/models/appointmentServiceModel';
+import { UpdatebasicrecordService } from './services/updatebasicrecord.service';
+import { FormBuilder } from '@angular/forms';
+
 @Component({
   selector: 'app-updateinfo',
   templateUrl: './updateinfo.component.html',
   styleUrls: ['./updateinfo.component.css']
 })
-export class UpdateinfoComponent {
-  constructor(private router: Router, private appointmentService : AppointmentServiceService,
-    @Inject(localStorageToken) private localStorage : any){}
 
-  disableSubmit : boolean = true;
-
+export class UpdateinfoComponent implements OnInit{
+  date: string;
+  constructor(private router: Router, private Updatebasicrecord:UpdatebasicrecordService, private fb: FormBuilder){
+    this.date = new Date().toISOString().slice(0, 16);
+  }
+  
   toppings = new FormControl('');
   toppingList: string[] = ['Extra cheese', 'Mushroom', 'Onion', 'Pepperoni', 'Sausage', 'Tomato'];
 
-  updateform = new FormGroup({
-    bp: new FormControl('', [Validators.required,Validators.pattern('^[1-9][0-9]{1,2}\\/[1-9][0-9]{1,2}$')]),
-    rate: new FormControl('', [Validators.required,Validators.pattern('^(6[0-9]|[7-9][0-9]|1[01][0-9]|120)$')]),
-    spo2: new FormControl('', [Validators.required,Validators.pattern('^(9[0-9]|100)%$')]),
-    height: new FormControl('',[Validators.required,Validators.pattern('^(6[0-9]|[7-9][0-9]|1[01][0-9]|150)$')]),
-    weight: new FormControl('', [Validators.required, Validators.pattern('^(6[0-9]|[7-9][0-9]|1[01][0-9]|350)$')]),
-    temp: new FormControl('', [Validators.required, Validators.pattern('^([3-9][0-9]|[1-9][0-9]{2})\.?[0-9]?$')]),
-    bloodgroup: new FormControl('',[Validators.required]),
-    // allergies: new FormControl('',[Validators.required]),
+  updateform !: FormGroup
+  alergyform !: FormGroup
+  
+  // updateform = this.fb.group({
+  //   bp: new FormControl('', [Validators.required,Validators.pattern('^[1-9][0-9]{1,2}\\/[1-9][0-9]{1,2}$')]),
+  //   heart_Rate: new FormControl('', [Validators.required,Validators.pattern('^(6[0-9]|[7-9][0-9]|1[01][0-9]|120)$')]),
+  //   spO2: new FormControl('', [Validators.required,Validators.pattern('^(9[0-9]|100)%$')]),
+  //   height: new FormControl('',[Validators.required,Validators.pattern('^(6[0-9]|[7-9][0-9]|1[01][0-9]|250)$')]),
+  //   weight: new FormControl('', [Validators.required, Validators.pattern('^(6[0-9]|[7-9][0-9]|1[01][0-9]|350)$')]),
+  //   temperature: new FormControl('', [Validators.required, Validators.pattern('^([3-9][0-9]|[1-9][0-9]{2})\.?[0-9]?$')]),
+  //   bloodGroup: new FormControl('',[Validators.required])
+  // });
 
-  });
-
-  updateNurseIdByNurse(){
-    this.appointmentService.updateNurseIdByNurse(this.localStorage.getItem('app_id for nurse'),'3245b75e-2398-4a8d-8617-44fa42f534c2')
-    .subscribe({
-      next : (appointment) => {
-        console.log(appointment);
-      },
-      error: (response) => {
-        console.log(response);
-      }
-
+  ngOnInit():void {
+    this.updateform = this.fb.group({
+      patient_Id: "p564",
+      nurse_Id : "n564",
+      appointment_Id : "a234",
+      date_Time: this.date,
+      bp: ['', [Validators.required,Validators.pattern('^[1-9][0-9]{1,2}\\/[1-9][0-9]{1,2}$')]],
+      heart_Rate: ['', [Validators.required,Validators.pattern('^(6[0-9]|[7-9][0-9]|1[01][0-9]|120)$')]],
+      spO2: ['', [Validators.required,Validators.pattern('^(9[0-9]|100)%$')]],
+      height: ['',[Validators.required,Validators.pattern('^(6[0-9]|[7-9][0-9]|1[01][0-9]|250)$')]],
+      weight: ['', [Validators.required, Validators.pattern('^(6[0-9]|[7-9][0-9]|1[01][0-9]|350)$')]],
+      temperature: ['', [Validators.required, Validators.pattern('^([3-9][0-9]|[1-9][0-9]{2})\.?[0-9]?$')]],
+      bloodGroup: ['',[Validators.required]]
     });
+    // this.alergyform = this.fb.group({
+    //   health_Id: "p564",
+    //   appointment_Id: "a234",
+    //   allergy: ['',[Validators.required]]
+    // })
   }
 
+  
 
-  update() {
-    console.log(this.updateform);
 
+  update(){
     if (this.updateform.valid) {
-
-      console.warn(this.updateform.value);
-      this.disableSubmit = false;
+      this.Updatebasicrecord.saveUser(this.updateform.getRawValue()).subscribe((result)=>{
+        console.log(result);
+      })
     } else {
       // show error message
       console.log(this.updateform);
       console.log('Invalid form');
     }
+    console.log(this.updateform.getRawValue())
   }
-
+ 
   goBack(){
     this.router.navigate(['nurse-dashboard'])
   }
@@ -67,12 +77,12 @@ export class UpdateinfoComponent {
     return this.updateform.get('bp');
   }
 
-  get rate() {
-    return this.updateform.get('rate');
+  get heart_Rate() {
+    return this.updateform.get('heart_Rate');
   }
 
-  get spo2() {
-    return this.updateform.get('spo2');
+  get spO2() {
+    return this.updateform.get('spO2');
   }
 
   get height() {
@@ -83,17 +93,12 @@ export class UpdateinfoComponent {
     return this.updateform.get('weight');
   }
 
-  get temp() {
-    return this.updateform.get('temp');
+  get temperature() {
+    return this.updateform.get('temperature');
   }
-  get bloodgroup(){
-    return this.updateform.get('bloodgroup')
+  get bloodGroup(){
+    return this.updateform.get('bloodGroup');
   }
-
-  // get allergies(){
-  //   return this.updateform.get('toppings')
-  // }
-
 
   getErrorMessage(formControlName: string) {
     if (this.updateform.get(formControlName)?.hasError('required')) {
@@ -101,22 +106,20 @@ export class UpdateinfoComponent {
     } else if (this.updateform.get(formControlName)?.hasError('pattern')) {
       if (formControlName === 'bp') {
         return 'Please enter a valid blood pressure reading in the format of systolic/diastolic (e.g. 120/80)';
-      } else if (formControlName === 'rate') {
+      } else if (formControlName === 'heart_Rate') {
         return 'Please enter a valid heart rate reading between 60 and 120 bpm';
-      } else if (formControlName === 'spo2') {
+      } else if (formControlName === 'spO2') {
         return 'Please enter a valid SpO2 reading between 90% and 100%';
       } else if (formControlName === 'height') {
         return 'Please enter a valid height reading like 126.8';
       } else if (formControlName === 'weight'){
         return 'Please enter a valid height reading like 90';
-      }else if (formControlName === 'temp'){
+      }else if (formControlName === 'temperature'){
         return 'Please enter a valid height reading like 37.1';
       }else{
         return 'Invalid input';
       }
     }
     return '';
-
-
   }
 }
