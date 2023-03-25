f218a25fd3f169611e52d80b830cc8ac41a2b62c
manojkmj11 <mithuna692@gmail.com>
2023-03-25 16:54:50 +0530

    completed doctor availability update service in admin module

--- complete-ui/src/app/components/admin/update-schedule/update-schedule.component.ts
+++ complete-ui/src/app/components/admin/update-schedule/update-schedule.component.ts
@@ -1,124 +1,135 @@
-import {SelectionModel} from '@angular/cdk/collections';
-import {Component, OnInit} from '@angular/core';
-import {MatTableDataSource} from '@angular/material/table';
+import { SelectionModel } from '@angular/cdk/collections';
+import { Component, OnInit } from '@angular/core';
+import { MatTableDataSource } from '@angular/material/table';
 // import { ConsoleReporter } from 'jasmine';
-import { Schedule } from '../../../schedule';
+// import { Schedule } from '../../../schedule';
+import { Schedule } from '../add-schedule/availability.service';
 import { AvailabilityService } from '../add-schedule/availability.service';
+import { GetAllDoctorsService } from './get-all-doctors.service';
+import { Doctor } from '../add-doctor/doctor';
 
-export interface PeriodicElement {
+export interface UpdateDoctor {
   name: string;
   position: number;
-  specialisation : string;
-  day : number;
+  specialisation: string;
+  day: number;
 }
 
-const ELEMENT_DATA: PeriodicElement[] = [
-  {position: 1, name: 'Mike', specialisation: "Cardiology", day : 0},
-  {position: 2, name: 'John', specialisation: "Cardiology", day : 0},
-  {position: 3, name: 'Josh', specialisation: "Cardiology", day : 0},
-  {position: 4, name: 'Mat', specialisation: "Cardiology", day : 0}
-];
+//  ELEMENT_DATA: UpdateDoctor[] = [
+//   {position: 1, name: 'Mike', specialisation: "Cardiology", day : 0},
+//   {position: 2, name: 'John', specialisation: "Cardiology", day : 0},
+//   {position: 3, name: 'Josh', specialisation: "Cardiology", day : 0},
+//   {position: 4, name: 'Mat', specialisation: "Cardiology", day : 0}
+// ];
 
 @Component({
   selector: 'app-update-schedule',
   templateUrl: './update-schedule.component.html',
-  styleUrls: ['./update-schedule.component.css']
+  styleUrls: ['./update-schedule.component.css'],
 })
 export class UpdateScheduleComponent implements OnInit {
-
-  constructor(private schedule : AvailabilityService) {}
+  constructor(
+    private schedule: AvailabilityService,
+    private getAllService: GetAllDoctorsService
+  ) {}
 
   currentDate = new Date();
-  weekDate !: Date;
-  allNotSelected !: boolean;
-  checked !: number[];
-  dayName !: string;
+  weekDate!: Date;
+  allNotSelected!: boolean;
+  checked!: number[];
+  dayName!: string;
 
-  schedules : Schedule[] = [{
-    doctorId : "0024ef20-8067-b43c-98c4-99486e793ceb",
-    Monday : 0,
-    Tuesday : 0,
-    Wednesday : 0,
-    Thursday : 0,
-    Friday : 0,
-    Saturday : 0,
-    Sunday : 0
-  },{
-    doctorId : "5c2ac33e-a085-4d36-76c0-3417df45fa42",
-    Monday : 0,
-    Tuesday : 0,
-    Wednesday : 0,
-    Thursday : 0,
-    Friday : 0,
-    Saturday : 0,
-    Sunday : 0
-  },{
-    doctorId : "2610c4cb-1c0b-d0f8-f47d-3952dd71c278",
-    Monday : 0,
-    Tuesday : 0,
-    Wednesday : 0,
-    Thursday : 0,
-    Friday : 0,
-    Saturday : 0,
-    Sunday : 0
-  },{
-    doctorId : "74759321-8d28-a3d3-b5b8-3ffd32276af2",
-    Monday : 0,
-    Tuesday : 0,
-    Wednesday : 0,
-    Thursday : 0,
-    Friday : 0,
-    Saturday : 0,
-    Sunday : 0
-  }]
+  schedules: Schedule[] = []
+
+  doctors: Doctor[] = [];
+
+  // doctors : Doctor[] = [{
+  //   name : "xcv",
+  //   email : "sdffggh",
+  //   phoneNo : 234665432,
+  //   imgUrl : "sdgfe",
+  //   specialization : 'hii',
+  //   experience : 23,
+  //   gender : 'male'
+  // },
+  // {
+  //   name : "xcv",
+  //   email : "sdffggh",
+  //   phoneNo : 234665432,
+  //   imgUrl : "sdgfe",
+  //   specialization : 'hii',
+  //   experience : 23,
+  //   gender : 'male'
+  // }]
+
+  ELEMENT_DATA: UpdateDoctor[] = [];
+  dataSource = new MatTableDataSource<UpdateDoctor>();
+  displayedColumns!: string[];
+  selection = new SelectionModel<UpdateDoctor>();
 
   ngOnInit() {
     this.weekDate = new Date(this.currentDate.getTime() + 604800000);
-    this.allNotSelected = false
-    this.checked = []
+    this.allNotSelected = false;
+    this.checked = [];
+    this.getAllService.getAll().subscribe((data) => {
+      this.doctors = data;
+      console.log(this.doctors);
+      this.doctors.forEach((doctor) => {
+        this.ELEMENT_DATA.push({
+          position: 1,
+          name: `${doctor.name}`,
+          specialisation: `${doctor.specialization}`,
+          day: 0,
+        });
+        this.schedules.push({doctorId : `${doctor.id}`, monday: 0, tuesday : 0, wednesday:0, thursday:0, friday:0, saturday:0, sunday:0})
+      });
+      console.log(this.schedules);
+
+      this.displayedColumns = ['position', 'name', 'specialisation', 'select'];
+      this.dataSource = new MatTableDataSource<UpdateDoctor>(
+        this.ELEMENT_DATA
+      );
+      this.selection = new SelectionModel<UpdateDoctor>(true, []);
+    });
   }
 
-  
-
-  displayedColumns: string[] = ['position', 'name', 'specialisation', 'select'];
-  dataSource = new MatTableDataSource<PeriodicElement>(ELEMENT_DATA);
-  selection = new SelectionModel<PeriodicElement>(true, []);
-
   updateSchedule() {
-    let dayNum = this.currentDate.getDay()
-    let i 
-    switch(dayNum){
-      case 0 :
-        console.log("Entered sunday");
-        for(i = 0; i<ELEMENT_DATA.length ; i++)
-          this.schedules[i].Sunday = ELEMENT_DATA[i].day
-          console.log(i)
-        break
-      case 1 :
-        for(i = 0; i<ELEMENT_DATA.length ; i++)
-          this.schedules[i].Monday = ELEMENT_DATA[i].day
-        break
-      case 2 :
-        for(i = 0; i<ELEMENT_DATA.length ; i++)
-          this.schedules[i].Tuesday = ELEMENT_DATA[i].day
-        break
-      case 3 :
-        for(i = 0; i<ELEMENT_DATA.length ; i++)
-          this.schedules[i].Wednesday = ELEMENT_DATA[i].day
-        break
-      case 4 :
-        for(i = 0; i<ELEMENT_DATA.length ; i++)
-          this.schedules[i].Thursday = ELEMENT_DATA[i].day
-        break
-      case 5 :
-        for(i = 0; i<ELEMENT_DATA.length ; i++)
-          this.schedules[i].Friday = ELEMENT_DATA[i].day
-        break
-      case 6 :
-        for(i = 0; i<ELEMENT_DATA.length ; i++)
-          this.schedules[i].Saturday = ELEMENT_DATA[i].day
-        break
+    let dayNum = this.currentDate.getDay();
+    let i;
+    switch (dayNum) {
+      case 0:
+        console.log('Entered sunday');
+        for (i = 0; i < this.ELEMENT_DATA.length; i++)
+          this.schedules[i].sunday = this.ELEMENT_DATA[i].day;
+        console.log(i);
+        break;
+      case 1:
+        for (i = 0; i < this.ELEMENT_DATA.length; i++)
+          this.schedules[i].monday = this.ELEMENT_DATA[i].day;
+        break;
+      case 2:
+        for (i = 0; i < this.ELEMENT_DATA.length; i++)
+          this.schedules[i].tuesday = this.ELEMENT_DATA[i].day;
+        break;
+      case 3:
+        for (i = 0; i < this.ELEMENT_DATA.length; i++)
+          this.schedules[i].wednesday = this.ELEMENT_DATA[i].day;
+        break;
+      case 4:
+        for (i = 0; i < this.ELEMENT_DATA.length; i++)
+          this.schedules[i].thursday = this.ELEMENT_DATA[i].day;
+        break;
+      case 5:
+        for (i = 0; i < this.ELEMENT_DATA.length; i++)
+          this.schedules[i].friday = this.ELEMENT_DATA[i].day;
+        break;
+      case 6:
+        for (i = 0; i < this.ELEMENT_DATA.length; i++)
+          this.schedules[i].saturday = this.ELEMENT_DATA[i].day;
+        break;
     }
-      this.schedule.UpdateDaySchedule(dayNum, this.schedules).subscribe(data => console.log(data));
+    this.schedule
+      .UpdateDaySchedule(dayNum, this.schedules)
+      .subscribe((data) => console.log(data));
   }
 }
